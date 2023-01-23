0e3181f4f6e538c8ee2ebd5c9a3db212df3be679
yunluchen <yunluchen@outlook.com>
2023-01-23 13:59:53 +0800

    BRG - Introduce SpringBoot as basement

--- src/main/java/org/haystack/Main.java
+++
@@ -1,7 +0,0 @@
-package org.haystack;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/haystack/bestbridge/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package org.haystack.bestbridge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Slf4j
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            log.info("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                log.info("Bean={}", beanName);
+            }
+        };
+    }
+}
